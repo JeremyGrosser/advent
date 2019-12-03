4eb8e7d223cd3842dd3bb1e5735724d02e58040e
Jeremy Grosser <jeremy@synack.me>
2019-12-02 21:30:32 -0800

    Day 1, Part 2

--- src/advent1.adb
+++ src/advent1.adb
@@ -5,15 +5,31 @@ with Ada.Strings.Unbounded.Text_IO; use Ada.Strings.Unbounded.Text_IO;
 procedure Advent1 is
     Line            : Unbounded_String;
     Mass            : Positive;
-    Fuel_Required   : Positive;
+    Fuel            : Natural;
     Sum             : Natural := 0;
+
+    function Fuel_Requirement (Mass : Positive) return Natural is
+        Fuel : Natural;
+    begin
+        Fuel := Mass / 3;
+        if Fuel >= 2 then
+            return (Fuel - 2);
+        else
+            return 0;
+        end if;
+    end Fuel_Requirement;
 begin
     loop
         exit when End_Of_File (Standard_Input);
         Line := Get_Line (Standard_Input);
         Mass := Positive'Value (To_String (Line));
-        Fuel_Required := ((Mass / 3) - 2);
-        Sum := Sum + Fuel_Required;
+
+        Fuel := Mass;
+        loop
+            exit when Fuel = 0;
+            Fuel := Fuel_Requirement (Fuel);
+            Sum := Sum + Fuel;
+        end loop;
     end loop;
 
     Put_Line (Standard_Output, Sum'Image);
