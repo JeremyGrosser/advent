72febe5b71413d06c18126c32f0e46eed72d1994
Jeremy Grosser <jeremy@synack.me>
2019-12-09 16:00:21 -0800

    Fix run procedure name

--- src/run.adb
+++ src/run.adb
@@ -1,11 +1,10 @@
 with Ada.Command_Line;
-
 with Intcode;
 
-procedure Advent5 is
+procedure Run is
     M : Intcode.Machine;
 begin
     M.Load_From_File(Ada.Command_Line.Argument (1));
     M.Reset;
     M.Run;
-end Advent5;
+end Run;
