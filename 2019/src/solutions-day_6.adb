2b674f0bc9f333cb9f64921c06fdbcda0f032795
Jeremy Grosser <jeremy@synack.me>
2020-11-14 08:23:20 -0800

    6.1

--- 2019/src/solutions-day_6.adb
+++ 2019/src/solutions-day_6.adb
@@ -4,6 +4,8 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Str; use Str;
 
 package body Solutions.Day_6 is
+   Not_Found : exception;
+
    package String_Vectors is new Ada.Containers.Indefinite_Vectors
       (Index_Type   => Positive,
        Element_Type => String);
@@ -65,12 +67,12 @@ package body Solutions.Day_6 is
       return Total;
    end Indirect_Orbits;
 
-   function Part_1 (Filename : String)
-      return Natural
+   function Read
+      (Filename : String)
+      return String_Maps.Map
    is
       Input    : File_Type;
       Star_Map : String_Maps.Map;
-      Total    : Natural := 0;
    begin
       Open (Input, In_File, Filename);
       loop
@@ -91,7 +93,16 @@ package body Solutions.Day_6 is
          end;
       end loop;
       Close (Input);
+      return Star_Map;
+   end Read;
 
+
+   function Part_1 (Filename : String)
+      return Natural
+   is
+      Star_Map : String_Maps.Map := Read (Filename);
+      Total    : Natural := 0;
+   begin
       for Cursor in Star_Map.Iterate loop
          declare
             Key : String := String_Maps.Key (Cursor);
@@ -101,6 +112,60 @@ package body Solutions.Day_6 is
       end loop;
       return Total;
    end Part_1;
+
+   function Path
+      (M            : String_Maps.Map;
+       Origin, Dest : String)
+       return String_Vectors.Vector
+   is
+      Vec : String_Vectors.Vector;
+   begin
+      if not M.Contains (Origin) then
+         return String_Vectors.Empty_Vector;
+      else
+         Vec := M.Element (Origin);
+      end if;
+
+      if Vec.Contains (Dest) then
+         return Vec;
+      else
+         for Cursor in Vec.Iterate loop
+            declare
+               use type Ada.Containers.Count_Type;
+               Key : String := String_Vectors.Element (Cursor);
+               V   : String_Vectors.Vector := Path (M, Key, Dest);
+            begin
+               if V.Length > 0 then
+                  return Vec & V;
+               end if;
+            end;
+         end loop;
+         return String_Vectors.Empty_Vector;
+      end if;
+   end Path;
+
+   procedure Print (V : String_Vectors.Vector) is
+   begin
+      for Cursor in V.Iterate loop
+         Put (String_Vectors.Element (Cursor) & ",");
+      end loop;
+      Put_Line ("");
+   end Print;
+
+   function Part_2 (Filename : String)
+      return Natural
+   is
+      use type Ada.Containers.Count_Type;
+      Star_Map : String_Maps.Map := Read (Filename);
+      From     : String_Vectors.Vector := Path (Star_Map, "COM", "YOU");
+      To       : String_Vectors.Vector := Path (Star_Map, "COM", "SAN");
+   begin
+      --Put ("From: ");
+      --Print (From);
+      --Put ("To:   ");
+      --Print (To);
+      return Natural (abs (From.Length - To.Length));
+   end Part_2;
 
    procedure Run is
       Result : Natural;
@@ -113,5 +178,14 @@ package body Solutions.Day_6 is
 
       Result := Part_1 ("input/advent6");
       Put_Line ("Solution 6.1:" & Result'Image);
+
+      Result := Part_2 ("input/advent6-example2");
+      if Result /= 4 then
+         Put_Line ("6.2 test failed, expected 4, got" & Result'Image);
+         return;
+      end if;
+
+      Result := Part_2 ("input/advent6");
+      Put_Line ("Solution 6.2:" & Result'Image);
    end Run;
 end Solutions.Day_6;
