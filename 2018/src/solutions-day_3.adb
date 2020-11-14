2aec07c78ce1703f4480789fd4d378f81fb5d446
Jeremy Grosser <jeremy@synack.me>
2020-11-14 06:32:46 -0800

    2018 3.2

--- 2018/src/solutions-day_3.adb
+++ 2018/src/solutions-day_3.adb
@@ -1,4 +1,5 @@
 with Ada.Containers.Vectors;
+with Ada.Containers.Ordered_Sets;
 with Ada.Text_IO; use Ada.Text_IO;
 
 package body Solutions.Day_3 is
@@ -126,16 +127,88 @@ package body Solutions.Day_3 is
       return Count;
    end Part_1;
 
-   procedure Run is
-      Result_1 : Integer;
+   function Part_2
+      (Filename    : String;
+       Fabric_Size : Positive)
+      return Integer
+   is
+      package Natural_Sets is new Ada.Containers.Ordered_Sets (Natural);
+      use type Natural_Sets.Set;
+      use type Ada.Containers.Count_Type;
+
+      Fabric     : Fabric_Array (0 .. Fabric_Size, 0 .. Fabric_Size) := (others => (others => 0));
+      Claims, Overlapped : Natural_Sets.Set;
+      Input_File : File_Type;
+      I : Natural;
    begin
-      Result_1 := Part_1 ("input/day3.1a", 8);
-      if Result_1 /= 4 then
-         Put_Line ("day3.1a failed: got " & Result_1'Image & ", expected 4");
+      Open (Input_File, In_File, Filename);
+
+      loop
+         exit when End_Of_File (Input_File);
+         declare
+            Line : constant String := Get_Line (Input_File);
+         begin
+            if Line (Line'First) /= '#' then
+               raise Syntax_Error with "Line does not begin with '#'";
+            end if;
+
+            declare
+               Claim_Id : constant Positive := Positive'Value (Split (Line (2 .. Line'Last), ' ', 0));
+               Coord    : constant String   := Strip (Split (Line, ' ', 2), ':');
+               X1       : constant Natural  := Natural'Value (Split (Coord, ',', 0));
+               Y1       : constant Natural  := Natural'Value (Split (Coord, ',', 1));
+               Size     : constant String   := Split (Line, ' ', 3);
+               X2       : constant Natural  := X1 + Natural'Value (Split (Size, 'x', 0)) - 1;
+               Y2       : constant Natural  := Y1 + Natural'Value (Split (Size, 'x', 1)) - 1;
+            begin
+               Claims.Include (Claim_Id);
+               for X in X1 .. X2 loop
+                  for Y in Y1 .. Y2 loop
+                     --Fabric (X, Y) := Fabric (X, Y) + 1;
+                     if Fabric (X, Y) /= 0 then
+                        Overlapped.Include (Fabric (X, Y));
+                        Overlapped.Include (Claim_Id);
+                     end if;
+                     Fabric (X, Y) := Claim_Id;
+                  end loop;
+               end loop;
+            end;
+         end;
+      end loop;
+
+      Close (Input_File);
+
+      Claims := Claims - Overlapped;
+      if Claims.Length = 0 then
+         Put_Line ("No non-overlapping claims found");
+         return 0;
+      elsif Claims.Length > 1 then
+         Put_Line ("Multiple non-overlapping claims found");
+         return 0;
+      else
+         return Claims.First_Element;
+      end if;
+   end Part_2;
+
+   procedure Run is
+      Result : Integer;
+   begin
+      Result := Part_1 ("input/day3.1a", 8);
+      if Result /= 4 then
+         Put_Line ("day3.1a failed: got " & Result'Image & ", expected 4");
          return;
       end if;
 
-      Result_1 := Part_1 ("input/day3", 1024);
-      Put_Line ("Solution 3.1: " & Result_1'Image);
+      Result := Part_1 ("input/day3", 1024);
+      Put_Line ("Solution 3.1: " & Result'Image);
+
+      Result := Part_2 ("input/day3.1a", 8);
+      if Result /= 3 then
+         Put_Line ("day3.2a failed: got " & Result'Image & ", expected 3");
+         return;
+      end if;
+
+      Result := Part_2 ("input/day3", 1024);
+      Put_Line ("Solution 3.2: " & Result'Image);
    end Run;
 end Solutions.Day_3;
--- 2018/src/solutions-day_3.ads
+++ 2018/src/solutions-day_3.ads
@@ -6,5 +6,10 @@ package Solutions.Day_3 is
        Fabric_Size : Positive)
        return Integer;
 
+   function Part_2
+      (Filename    : String;
+       Fabric_Size : Positive)
+       return Integer;
+
    procedure Run;
 end Solutions.Day_3;
