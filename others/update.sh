21b9f1a10748d1a0b7d7c0f05187a790e32a3cc7
Jeremy Grosser <jeremy@synack.me>
2020-12-02 10:00:04 -0800

    Script for tracking other users' solutions

---
+++ others/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for url in `cat repolist`; do
+    username=$(echo "${url}" | cut -d'/' -f4)
+    if [ ! -e ${username} ]; then
+        git clone ${url} ${username}
+    fi
+    echo "--------------------------------------------------------------------------------"
+    echo "${username}"
+    pushd ${username} >/dev/null
+    git fetch -q
+    git diff origin/$(git branch --show-current)
+    git pull -q -s recursive -X theirs origin $(git branch --show-current)
+    popd >/dev/null
+done
