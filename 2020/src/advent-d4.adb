3f240cfb9c84aa526c45c25a45c0865471f60b6b
Jeremy Grosser <jeremy@synack.me>
2020-12-04 09:31:36 -0800

    day 4 part 2

--- 2020/src/advent-d4.adb
+++ 2020/src/advent-d4.adb
@@ -1,62 +1,177 @@
-with Ada.Containers.Indefinite_Ordered_Sets;
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Assertions; use Ada.Assertions;
+with Ada.Characters.Handling;
 with Str; use Str;
 
 package body Advent.D4 is
    function Read_Until_Space
-      (F : File_Type)
+      (Input : File_Type)
       return String
    is
       Ch : Character;
    begin
       loop
-         exit when End_Of_File (F);
-         Get_Immediate (F, Ch);
+         exit when End_Of_File (Input);
+         Get_Immediate (Input, Ch);
          exit when Ch = ' ' or Ch = ASCII.LF;
-         return Ch & Read_Until_Space (F);
+         return Ch & Read_Until_Space (Input);
       end loop;
       return "";
    end Read_Until_Space;
 
    function Read_Passport
       (Input : File_Type)
-      return Boolean
+      return Passport
    is
-      package String_Sets is new Ada.Containers.Indefinite_Ordered_Sets (String);
-      Token : String (1 .. 32);
-      Seen  : String_Sets.Set;
+      P : Passport := Empty_Passport;
    begin
       loop
          declare
-            Token : String := Read_Until_Space (Input);
-            Key   : String := Split (Token, ':', 0);
-            Value : String := Split (Token, ':', 0);
+            Token : constant String := Read_Until_Space (Input);
+            Key   : constant String := Split (Token, ':', 0);
+            Value : constant String := Split (Token, ':', 1);
          begin
             if Token'Length = 0 then
-               return Seen.Contains ("byr") and
-                      Seen.Contains ("iyr") and
-                      Seen.Contains ("eyr") and
-                      Seen.Contains ("hgt") and
-                      Seen.Contains ("hcl") and
-                      Seen.Contains ("ecl") and
-                      Seen.Contains ("pid");
+               return P;
             else
-               Seen.Include (Key);
+               P.Insert (Key, Value);
             end if;
          end;
       end loop;
    end Read_Passport;
 
-   function Part_1 (Filename : in String)
+   function Has_Required_Fields
+      (P : Passport)
+      return Boolean
+   is
+   begin
+      return P.Contains ("byr") and then
+             P.Contains ("iyr") and then
+             P.Contains ("eyr") and then
+             P.Contains ("hgt") and then
+             P.Contains ("hcl") and then
+             P.Contains ("ecl") and then
+             P.Contains ("pid");
+   end Has_Required_Fields;
+
+   function Check_Number_Range
+      (S           : String;
+       First, Last : Integer)
+      return Boolean
+   is
+      N : constant Integer := Integer'Value (S);
+   begin
+      return N >= First and N <= Last;
+   end Check_Number_Range;
+
+   function Check_Height
+      (S : String)
+      return Boolean
+   is
+   begin
+      if S'Length < 4 then
+         return False;
+      end if;
+
+      declare
+         Units : constant String := S (S'Last - 1 .. S'Last);
+         N     : constant Integer := Integer'Value (S (S'First .. S'Last - 2));
+      begin
+         if Units = "in" then
+            return N >= 59 and N <= 76;
+         elsif Units = "cm" then
+            return N >= 150 and N <= 193;
+         else
+            return False;
+         end if;
+      end;
+   end Check_Height;
+
+   function Check_Hair
+      (S : String)
+      return Boolean
+   is
+      Valid_Chars : constant String := "0123456789abcdef";
+   begin
+      if S'Length /= 7 then
+         return False;
+      end if;
+
+      if S (S'First) /= '#' then
+         return False;
+      end if;
+
+      for I in S'First + 1 .. S'Last loop
+         if Find (Valid_Chars, S (I)) = 0 then
+            return False;
+         end if;
+      end loop;
+      return True;
+   end Check_Hair;
+
+   function Check_Eyes
+      (S : String)
+      return Boolean
+   is
+      type Color_Array is array (Positive range <>) of String (1 .. 3);
+      Valid_Colors : constant Color_Array :=
+         ("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+   begin
+      if S'Length /= 3 then
+         return False;
+      end if;
+      for Color of Valid_Colors loop
+         if S = Color then
+            return True;
+         end if;
+      end loop;
+      return False;
+   end Check_Eyes;
+
+   function Check_Passport_Id
+      (S : String)
+      return Boolean
+   is
+   begin
+      if S'Length /= 9 then
+         return False;
+      end if;
+
+      for C of S loop
+         if not Ada.Characters.Handling.Is_Digit (C) then
+            return False;
+         end if;
+      end loop;
+      return True;
+   end Check_Passport_Id;
+
+   function Validate
+      (P : Passport)
+      return Boolean
+   is
+   begin
+      return Has_Required_Fields (P) and then
+             Check_Number_Range (P.Element ("byr"), 1920, 2002) and then
+             Check_Number_Range (P.Element ("iyr"), 2010, 2020) and then
+             Check_Number_Range (P.Element ("eyr"), 2020, 2030) and then
+             Check_Height (P.Element ("hgt")) and then
+             Check_Hair (P.Element ("hcl")) and then
+             Check_Eyes (P.Element ("ecl")) and then
+             Check_Passport_Id (P.Element ("pid"));
+   end Validate;
+
+   function Part_1
+      (Filename : in String)
       return Integer
    is
       Input : File_Type;
       Valid : Natural := 0;
+      P     : Passport;
    begin
       Open (Input, In_File, Filename);
       loop
          exit when End_Of_File (Input);
-         if Read_Passport (Input) then
+         P := Read_Passport (Input);
+         if Has_Required_Fields (P) then
             Valid := Valid + 1;
          end if;
       end loop;
@@ -67,8 +182,20 @@ package body Advent.D4 is
    function Part_2 (Filename : in String)
       return Integer
    is
+      Input : File_Type;
+      Valid : Natural := 0;
+      P     : Passport;
    begin
-       return 0;
+      Open (Input, In_File, Filename);
+      loop
+         exit when End_Of_File (Input);
+         P := Read_Passport (Input);
+         if Validate (P) then
+            Valid := Valid + 1;
+         end if;
+      end loop;
+      Close (Input);
+      return Valid;
    end Part_2;
 
    procedure Run is
@@ -76,7 +203,23 @@ package body Advent.D4 is
       Test (Part_1'Access, "4.1", "input/d4-test", 2);
       Put_Line ("4.1 solution: " & Part_1 ("input/d4")'Image);
 
-      Test (Part_2'Access, "4.2", "input/d4-test", 99);
+      Assert (Check_Number_Range ("1921", 1920, 2002) = True);
+      Assert (Check_Number_Range ("1900", 1920, 2002) = False);
+      Assert (Check_Height ("21cm") = False);
+      Assert (Check_Height ("180cm") = True);
+      Assert (Check_Height ("21in") = False);
+      Assert (Check_Height ("59in") = True);
+      Assert (Check_Hair ("#0fffff") = True);
+      Assert (Check_Hair ("fffffff") = False);
+      Assert (Check_Hair ("#ABCDEF") = False);
+      Assert (Check_Eyes ("amb") = True);
+      Assert (Check_Eyes ("AMB") = False);
+      Assert (Check_Passport_Id ("087499704") = True);
+      Assert (Check_Passport_Id ("08749970?") = False);
+
+      Test (Part_2'Access, "4.2 T1", "input/d4.2-test1", 1);
+      Test (Part_2'Access, "4.2 T2", "input/d4.2-test2", 0);
+      Test (Part_2'Access, "4.2 T3", "input/d4.2-test3", 4);
       Put_Line ("4.2 solution: " & Part_2 ("input/d4")'Image);
    end Run;
 end Advent.D4;
--- 2020/src/advent-d4.ads
+++ 2020/src/advent-d4.ads
@@ -1,3 +1,41 @@
+with Ada.Containers.Indefinite_Ordered_Maps;
+with Ada.Text_IO; use Ada.Text_IO;
+
 package Advent.D4 is
+   package String_Maps is new Ada.Containers.Indefinite_Ordered_Maps
+      (String, String);
+
+   subtype Passport is String_Maps.Map;
+   Empty_Passport : constant Passport := String_Maps.Empty_Map;
+
+   function Read_Until_Space
+      (Input : File_Type)
+      return String;
+
+   function Read_Passport
+      (Input : File_Type)
+      return Passport;
+
+   function Has_Required_Fields
+      (P : Passport)
+      return Boolean;
+
+   function Check_Number_Range
+      (S           : String;
+       First, Last : Integer)
+      return Boolean;
+
+   function Check_Height
+      (S : String)
+      return Boolean;
+
+   function Check_Hair
+      (S : String)
+      return Boolean;
+
+   function Validate
+      (P : Passport)
+      return Boolean;
+
    procedure Run;
 end Advent.D4;
