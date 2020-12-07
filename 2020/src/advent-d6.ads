fe181bd27af63c63539b08a13940704e7899688d
Jeremy Grosser <jeremy@synack.me>
2020-12-06 20:52:22 -0800

    style

--- 2020/src/advent-d6.ads
+++ 2020/src/advent-d6.ads
@@ -2,8 +2,8 @@ package Advent.D6 is
    subtype Index_Type is Character range 'a' .. 'z';
    type Flag_Array is array (Index_Type) of Natural;
 
-   -- This isn't anything like a popcount, but it was the only name that came
-   -- to me at the time.
+   --  This isn't anything like a popcount, but it was the only name that came
+   --  to me at the time.
    function Popcount
       (FA    : Flag_Array;
        Match : Natural := 1)
