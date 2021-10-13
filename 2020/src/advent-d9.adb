1a3c50b56698b6944860ba077ad6a32ed198fcf0
Jeremy Grosser <jeremy@synack.me>
2021-10-12 23:06:22 -0700

    9.2

--- 2020/src/advent-d9.adb
+++ 2020/src/advent-d9.adb
@@ -1,9 +1,9 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 package body Advent.D9 is
-   function Valid
-      (Numbers : Integer_Vectors.Vector;
-       N       : Integer)
+   function Valid_1
+      (Numbers : Long_Integer_Vectors.Vector;
+       N       : Long_Integer)
        return Boolean
    is
    begin
@@ -15,26 +15,26 @@ package body Advent.D9 is
          end loop;
       end loop;
       return False;
-   end Valid;
+   end Valid_1;
 
    function Part_1
       (Filename        : String;
        Preamble_Length : Positive)
-      return Integer
+      return Long_Integer
    is
-      Numbers : Integer_Vectors.Vector := Integer_Vectors.Empty_Vector;
+      Numbers : Long_Integer_Vectors.Vector := Long_Integer_Vectors.Empty_Vector;
       Input   : File_Type;
    begin
       Open (Input, In_File, Filename);
       loop
          exit when End_Of_File (Input);
          declare
-            use Integer_Vectors;
-            N        : constant Integer := Integer'Value (Get_Line (Input));
+            use Long_Integer_Vectors;
+            N        : constant Long_Integer := Long_Integer'Value (Get_Line (Input));
             Preamble : constant Boolean := (Natural (Length (Numbers)) <= Preamble_Length);
          begin
             if not Preamble then
-               if not Valid (Numbers, N) then
+               if not Valid_1 (Numbers, N) then
                   --  Put_Line (N'Image & " is not a sum of two of the previous " & Preamble_Length'Image & " numbers");
                   Close (Input);
                   return N;
@@ -49,9 +49,81 @@ package body Advent.D9 is
       return 0;
    end Part_1;
 
+   function Read_Numbers
+      (Filename : String)
+      return Int_Array
+   is
+      use Long_Integer_Vectors;
+      Numbers : Vector := Empty_Vector;
+      Input   : File_Type;
+   begin
+      Open (Input, In_File, Filename);
+      loop
+         exit when End_Of_File (Input);
+         declare
+            N : constant Long_Integer := Long_Integer'Value (Get_Line (Input));
+         begin
+            Append (Numbers, N);
+         end;
+      end loop;
+
+      declare
+         Result : Int_Array (1 .. Last_Index (Numbers));
+      begin
+         for I in Result'Range loop
+            Result (I) := Element (Numbers, I);
+         end loop;
+         return Result;
+      end;
+   end Read_Numbers;
+
+   function Valid_2
+      (Numbers     : Int_Array;
+       First, Last : Positive)
+       return Result_Type
+   is
+      R : Result_Type;
+      N : Long_Integer;
+   begin
+      for I in First .. Last loop
+         N := Numbers (I);
+         if N < R.Smallest then
+            R.Smallest := N;
+         elsif N > R.Largest then
+            R.Largest := N;
+         end if;
+         R.Sum := R.Sum + N;
+      end loop;
+      return R;
+   end Valid_2;
+
+   function Part_2
+      (Filename        : String;
+       Preamble_Length : Positive)
+      return Long_Integer
+   is
+      Target   : constant Long_Integer := Part_1 (Filename, Preamble_Length);
+      Numbers  : constant Int_Array := Read_Numbers (Filename);
+      R        : Result_Type;
+   begin
+      for Last in Numbers'First + 1 .. Numbers'Last loop
+         for First in Numbers'First .. Last - 1 loop
+            R := Valid_2 (Numbers, First, Last);
+            if R.Sum = Target then
+               return R.Smallest + R.Largest;
+            end if;
+         end loop;
+      end loop;
+
+      return 0;
+   end Part_2;
+
    procedure Run is
    begin
       pragma Assert (Part_1 ("input/d9-test", 5) = 127);
       Put_Line ("9.1 solution: " & Part_1 ("input/d9", 25)'Image);
+
+      pragma Assert (Part_2 ("input/d9-test", 5) = 62);
+      Put_Line ("9.2 solution: " & Part_2 ("input/d9", 25)'Image);
    end Run;
 end Advent.D9;
--- 2020/src/advent-d9.ads
+++ 2020/src/advent-d9.ads
@@ -1,19 +1,41 @@
 with Ada.Containers.Vectors;
 
 package Advent.D9 is
-   package Integer_Vectors is new Ada.Containers.Vectors
+   package Long_Integer_Vectors is new Ada.Containers.Vectors
       (Index_Type   => Positive,
-       Element_Type => Integer);
+       Element_Type => Long_Integer);
 
-   function Valid
-      (Numbers : Integer_Vectors.Vector;
-       N       : Integer)
+   function Valid_1
+      (Numbers : Long_Integer_Vectors.Vector;
+       N       : Long_Integer)
        return Boolean;
 
    function Part_1
       (Filename        : String;
        Preamble_Length : Positive)
-      return Integer;
+      return Long_Integer;
+
+   type Int_Array is array (Positive range <>) of Long_Integer;
+
+   function Read_Numbers
+      (Filename : String)
+      return Int_Array;
+
+   type Result_Type is record
+      Sum      : Long_Integer := 0;
+      Smallest : Long_Integer := Long_Integer'Last;
+      Largest  : Long_Integer := Long_Integer'First;
+   end record;
+
+   function Valid_2
+      (Numbers     : Int_Array;
+       First, Last : Positive)
+       return Result_Type;
+
+   function Part_2
+      (Filename        : String;
+       Preamble_Length : Positive)
+      return Long_Integer;
 
    procedure Run;
 end Advent.D9;
