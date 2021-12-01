9d024f73cf1e0558c30d2160a034ef7b56c1deba
Jeremy Grosser <jeremy@synack.me>
2021-11-30 21:15:42 -0800

    d1.1

--- 2021/src/d1.adb
+++ 2021/src/d1.adb
@@ -1,15 +1,38 @@
 with Advent_IO; use Advent_IO;
-with Ada.Command_Line;
+with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 
 procedure D1 is
-   procedure Part_1 is
-      Line : constant String := Read_Until (ASCII.LF);
+
+   function Get_Integer
+      return Integer
+   is
+      Line : constant String := Read_Until (Whitespace);
    begin
-      String'Write (Standard_Output, Line);
-      New_Line;
-      Flush;
+      if Line'Length > 1 then
+         return Integer'Value (Line);
+      else
+         raise End_Error;
+      end if;
+   end Get_Integer;
+
+   procedure Part_1 is
+      Current, Previous : Integer;
+      Increases : Natural := 0;
+   begin
+      Previous := Get_Integer;
+      loop
+         Current := Get_Integer;
+         if Current > Previous then
+            Increases := Increases + 1;
+         end if;
+         Previous := Current;
+      end loop;
+   exception
+      when End_Error =>
+         String'Write (Standard_Output, Increases'Image);
+         New_Line;
+         Flush;
    end Part_1;
 begin
    Part_1;
-   Ada.Command_Line.Set_Exit_Status (123);
 end D1;
--- 2021/test.sh
+++ 2021/test.sh
@@ -2,5 +2,6 @@
 PATH=$PATH:$PWD/bin
 set -e -x
 alr --no-tty -q build
-d1 < inputs/d1-test
+
+#d1 < inputs/d1-test
 d1 < inputs/d1
