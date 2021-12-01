d7f99b270936d2e8139742a8216de15ce2e01623
Jeremy Grosser <jeremy@synack.me>
2021-12-01 03:01:40 -0800

    style for adacontrol

--- 2021/src/d1_2.adb
+++ 2021/src/d1_2.adb
@@ -2,7 +2,8 @@ with Advent_IO; use Advent_IO;
 with Advent_IO.Generic_Numbers;
 
 procedure D1_2 is
-   package Integer_IO is new Advent_IO.Generic_Numbers (Integer);
+   package Integer_IO is new Advent_IO.Generic_Numbers
+      (Number => Integer);
    use Integer_IO;
 
    Items    : constant Numbers := Get (Input);
