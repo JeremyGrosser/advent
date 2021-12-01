3c3b40f5c3312e24b00521c653266e55e0308cc2
Jeremy Grosser <jeremy@synack.me>
2021-12-01 01:56:35 -0800

    First is a bad name for that.

--- 2021/src/d1_2.adb
+++ 2021/src/d1_2.adb
@@ -6,21 +6,21 @@ procedure D1_2 is
    use Integer_IO;
 
    Items    : constant Numbers := Get (Input);
-   First    : Positive := Items'First + 2;
+   I        : Positive := Items'First + 2;
    Sum      : Integer;
    Previous : Integer := 0;
    Result   : Natural := 0;
 begin
-   while First <= Items'Last loop
+   while I <= Items'Last loop
       Sum := 0;
-      for I in First - 2 .. First loop
-         Sum := Sum + Items (I);
+      for J in I - 2 .. I loop
+         Sum := Sum + Items (J);
       end loop;
       if Sum > Previous then
          Result := Result + 1;
       end if;
       Previous := Sum;
-      First := First + 1;
+      I := I + 1;
    end loop;
    Put (Output, Result - 1); --  subtract 1 because the first result doesn't count
    New_Line (Output);
