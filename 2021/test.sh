eb6dcac7983c29ca6d469ce7f2c44f1602ba78e1
Jeremy Grosser <jeremy@synack.me>
2021-12-01 01:31:25 -0800

    Be a little clever about testing solutions

--- 2021/test.sh
+++ 2021/test.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 PATH=$PATH:$PWD/bin
-set -e -x
+set -e
 alr --no-tty build
 
-d1_1 < inputs/d1-test
+assert() {
+    binary=$1
+    input=$2
+    expectation=$3
+    result=`$binary < $input`
+    if [ $result != $expectation ]; then
+        echo -e "\e[31mFAIL\e[0m $binary < $input"
+        echo "Expected: $expectation"
+        echo "Result:   $result"
+        exit 1
+    fi
+}
+
+assert d1_1 inputs/d1-test 7
+echo -n "1.1 solution: "
 d1_1 < inputs/d1
-d1_2 < inputs/d1-test
+
+assert d1_2 inputs/d1-test 5
+echo -n "1.2 solution: "
 d1_2 < inputs/d1
