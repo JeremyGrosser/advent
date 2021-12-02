c724ad72a9979024136ec3b0b8a5a70fd8080d84
Jeremy Grosser <jeremy@synack.me>
2021-12-01 21:45:27 -0800

    2.1

--- 2021/test.sh
+++ 2021/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 PATH=$PATH:$PWD/bin
 set -e
 alr --no-tty build
@@ -9,7 +9,7 @@ assert() {
     expectation=$3
     result=`$binary < $input`
     if [ $result != $expectation ]; then
-        echo " \e[1m\e[31mFAIL\e[0m $binary < $input"
+        echo -e " \e[1m\e[31mFAIL\e[0m $binary < $input"
         echo "Expected: $expectation"
         echo "Result:   $result"
         return 1
@@ -20,7 +20,7 @@ solve() {
     binary=$1
     input=$2
     result=`$binary < $input`
-    echo " \e[1m\e[92m$result\e[0m"
+    echo -e " \e[1m\e[92m$result\e[0m"
 }
 
 echo -n "1.1"
@@ -30,3 +30,7 @@ solve d1_1 inputs/d1
 echo -n "1.2"
 assert d1_2 inputs/d1-test 5
 solve d1_2 inputs/d1
+
+echo -n "2.1"
+assert d2_1 inputs/d2-test 150
+solve d2_1 inputs/d2
