f7e8794c088cb59fd81a05df01b90b87d91c5ab9
Jeremy Grosser <jeremy@synack.me>
2022-12-01 15:47:54 -0800

    note correct answer for jumbo puzzle and expected hash

--- 2022/input/gen_jumbo_day1.py
+++ 2022/input/gen_jumbo_day1.py
@@ -6,3 +6,8 @@ with open('day1_jumbo', 'w') as fd:
         for j in range(random.randint(3, 10)):
             fd.write(str(random.randint(1, 8) * 1000) + '\n')
         fd.write('\n')
+
+# The correct solution is 227000
+
+# sha1sum day1_jumbo
+# c73e8435027c2eefc5981016ca33158143210008  day1_jumbo
