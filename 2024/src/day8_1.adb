01bf91f2b34200095b80e6bf0a62805fa2086a93
Jeremy Grosser <jeremy@synack.me>
2024-12-08 12:01:27 -0800

    cleanup

--- 2024/src/day8_1.adb
+++ 2024/src/day8_1.adb
@@ -1,14 +1,9 @@
-pragma Warnings (Off, "array aggregate using () is an obsolescent syntax, use [] instead");
-pragma Ada_2022;
-with Ada.Containers.Ordered_Sets;
 with Ada.Containers.Vectors;
-with Ada.Text_IO;
 with Advent; use Advent;
 with Advent.Input;
 with Advent.Output;
 
 procedure Day8_1 is
-
    Width  : constant Natural := Input.Read_Until (ASCII.LF)'Length;
    Height : constant Natural := Input.Length / Width;
 
@@ -23,24 +18,18 @@ procedure Day8_1 is
       return Boolean
    is (C.Y in 0 .. Height - 1 and then C.X in 0 .. Width - 1);
 
-   function "<" (Left, Right : Coordinate) return Boolean
-   is (Left.Y < Right.Y or else Left.X < Right.X);
-
-   function "=" (Left, Right : Coordinate) return Boolean
-   is (Left.Y = Right.Y and then Left.X = Right.X);
-
    function "-" (Left, Right : Coordinate) return Coordinate
    is ((Left.Y - Right.Y, Left.X - Right.X));
 
    function "*" (Left : Integer; Right : Coordinate) return Coordinate
    is ((Left * Right.Y, Left * Right.X));
 
-   package Coordinate_Sets is new Ada.Containers.Vectors (Positive, Coordinate);
-   use Coordinate_Sets;
-   type Frequency_Map is array (Frequency) of Coordinate_Sets.Vector;
+   package Coordinate_Vectors is new Ada.Containers.Vectors (Positive, Coordinate);
+   use Coordinate_Vectors;
+   type Frequency_Map is array (Frequency) of Coordinate_Vectors.Vector;
 
    Antennas  : Frequency_Map;
-   Antinodes : Coordinate_Sets.Vector;
+   Antinodes : Coordinate_Vectors.Vector;
 
    procedure Add_Antinodes
       (A, B : Coordinate)
@@ -52,49 +41,13 @@ procedure Day8_1 is
       end if;
 
       Pos := 2 * A - B;
-      --  if In_Bounds (Pos) then
       if In_Bounds (Pos) and then not Contains (Antinodes, Pos) then
          Append (Antinodes, Pos);
       end if;
-      --  end if;
-
-      Pos := 2 * B - A;
-      --  if In_Bounds (Pos) then
-      if In_Bounds (Pos) and then not Contains (Antinodes, Pos) then
-         Append (Antinodes, Pos);
-      end if;
-      --  end if;
    end Add_Antinodes;
-
-   procedure Plot
-      (S : Coordinate_Sets.Vector)
-   is
-      Map : array (0 .. Height - 1, 0 .. Width - 1) of Natural := (others => (others => 0));
-   begin
-      --  for Freq in Frequency'Range loop
-      --     for Pos of Antennas (Freq) loop
-      --        Map (Pos.Y, Pos.X) := @ + 1;
-      --     end loop;
-      --  end loop;
-
-      for Pos of S loop
-         if In_Bounds (Pos) then
-            Map (Pos.Y, Pos.X) := @ + 1;
-         end if;
-      end loop;
-
-      for Y in Map'Range (1) loop
-         for X in Map'Range (2) loop
-            Ada.Text_IO.Put (Ada.Text_IO.Standard_Error, Character'Val (Character'Pos ('0') + Map (Y, X)));
-         end loop;
-         Ada.Text_IO.New_Line (Ada.Text_IO.Standard_Error);
-      end loop;
-      Ada.Text_IO.New_Line (Ada.Text_IO.Standard_Error);
-   end Plot;
 
    Pos : Coordinate := (0, 0);
    Ch  : Character;
-   Sum : Natural := 0;
 begin
    Input.Seek (0, Input.Seek_Start);
 
@@ -116,23 +69,9 @@ begin
       for A of Antennas (Freq) loop
          for B of Antennas (Freq) loop
             Add_Antinodes (A, B);
-            --  Add_Antinodes (B, A);
          end loop;
       end loop;
    end loop;
 
-   for Pos of Antinodes loop
-      if In_Bounds (Pos) then
-         Output.Log (Pos'Image);
-         Sum := Sum + 1;
-      else
-         Output.Log ("OOB: " & Pos'Image);
-      end if;
-   end loop;
-
-   Output.Log (Natural (Length (Antinodes)));
-
-   Plot (Antinodes);
-
-   Output.Put (Sum);
+   Output.Put (Natural (Length (Antinodes)));
 end Day8_1;
