e7044014ff0bd4883e2210fab6c557d63777bdce
Jeremy Grosser <jeremy@synack.me>
2024-12-08 12:45:12 -0800

    slightly more efficient day8 jumbo

--- 2024/jumbo/day8.py
+++ 2024/jumbo/day8.py
@@ -4,8 +4,8 @@ jumbo = []
 with open('../input/day8', 'r') as orig:
     with open('../input/day8.jumbo', 'w') as output:
         for line in orig.readlines():
-            jumbo.append(line[:-1] * repeat_x)
+            jumbo.append(line.rstrip('\n'))
         for i in range(repeat_y):
             for line in jumbo:
-                output.write(line)
+                output.write(line * repeat_x)
                 output.write('\n')
